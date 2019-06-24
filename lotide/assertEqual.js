fb607b1c7c3e5aace045b180a0b1f3a343754c46
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-06-24 19:50:40 +0000

    assertEqual done.

---
+++ lotide/assertEqual.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  return (actual === expected ?
+  console.log("✅✅✅ Assertion Passed: ", actual, " === ", expected)
+  : console.log("❌❌❌ Assertion Failed: ", actual, " !== ", expected));
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
