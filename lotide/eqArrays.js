5667faec8e777108b704eedcaa0e0987cfb179d0
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-06-25 21:43:13 +0000

    additional fix

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -1,11 +1,7 @@
-const assertEqual = function(actual, expected) {
-  return (actual === expected ?
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
-};
-
 const eqArrays = function(arr1, arr2) {
-  for (i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
   } return true;
-}
+};
+
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
