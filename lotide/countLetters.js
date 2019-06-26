0fe5470038f299d03b9d8eedd4f0b674aeb1e34e
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-06-26 18:49:57 +0000

    hame

---
+++ lotide/countLetters.js
@@ -0,0 +1,27 @@
+const countLetters = function(WordsToCount) {
+  // turn the string into an array;
+  const arrayToCount = WordsToCount.split('');
+  // console.log(arrayToCount);
+    // build an empty Object;
+  const newArray = [];
+  for (let i = 0; i < arrayToCount.length; i++) {
+    if (arrayToCount[i] !== "," && arrayToCount[i] !== "." && arrayToCount[i] !== " ") {
+      newArray.push(arrayToCount[i]);
+    } 
+  }
+  // console.log(newArray);
+
+  const letterCounts = {};
+  //for each letter, two steps, give it a key, then add 1 to it if sees again;
+  for (const letter of newArray) {
+    // console.log(letter);
+    if (letterCounts[letter]) {
+      letterCounts[letter] += 1;
+    } else {
+      letterCounts[letter] = 1;
+    }
+  } 
+  return letterCounts;
+};
+
+console.log(countLetters("Hello, olleH."));
