761fb7a76189433c63eca61cff88971df4187e13
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-06-30 17:52:32 +0000

    fixed bugs

--- lotide/eqObjects.js
+++ lotide/eqObjects.js
@@ -10,27 +10,50 @@ const eqObjects = function(object1, object2) {
     return false;
   
   } else {
-  
-        for (let key in object1){
-          // console.log(key);}}
-  
-          if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-            if(object1[key].length === object2[key].length) {
-              for (i = 0; i < object1[key].length; i++) {
-                if (object1[key][i] === object2[key][i]) {
-                  // console.log(object1[key][i] !== object2[key][i]);
-                  return true;
+    for (let key in object1) {
+
+      if (typeof(object1[key]) !== typeof(object2[key])) {
+        return false;
+
+      } else {
+        
+        for (let key in object1) {
+          if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+
+            if(object1[key] !== object2[key]) {
+              return false;
+
+            } else {
+
+              if (object1[key].length !== object2[key].length) {
+                return false;
+
+              } else {
+
+                for (i = 0; i < object1[key].length; i++) {
+                  if (object1[key][i] !== object2[key][i]) {
+                    return false;
+
+                  } return true;
                 }
               }
             }
           }
-        } 
-      } 
-  };
+        }
+      }
+    }
+  }
+};
   
   
   const cd = { c: "1", d: ["2", 3] };
   const dc = { d: ["2", 3], c: "1" };
+
+//   // FUNCTION IMPLEMENTATION
+// const assertObjectsEqual = function(actual, expected) {
+//   // Implement me!
+// };
+
   eqObjects(cd, dc);
   console.log(eqObjects(cd, dc)); // => true
   
