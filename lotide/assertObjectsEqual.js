2e0ef859f96daab0bb89851547b70e5cd6353217
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-06-30 21:58:07 +0000

    still not clear about the instruction, what exactly I should do?

---
+++ lotide/assertObjectsEqual.js
@@ -0,0 +1,51 @@
+const cd = { c: "1", d: ["2", 3] }; 
+const dc = { d: ["2", 3], c: "1" };
+
+const eqObjects = function(object1, object2) {
+
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  
+  // console.log(keys1, keys2);
+  
+  if (keys1.length !== keys2.length) {
+  
+    return false;
+  
+  } else {
+    for (let key in object1) {
+
+      if (typeof(object1[key]) !== typeof(object2[key])) {
+        return false;
+
+      } else {
+        
+        for (let key in object1) {
+          if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+
+            if (object1[key].length !== object2[key].length) {
+              return false;
+
+            } else {
+
+              for (i = 0; i < object1[key].length; i++) {
+
+                if (object1[key][i] !== object2[key][i]) {
+                  return false;
+                }
+              } return true;
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+
+const assertObjectsEqual = function(actual, expected) {
+  return (actual === expected ?
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
+};
+console.log(assertObjectsEqual(eqObjects(cd,dc), false));
