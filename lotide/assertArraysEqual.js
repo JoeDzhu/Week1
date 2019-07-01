3c72450b270d7c99df74f670a1311d8e601bd33d
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-07-01 17:18:46 +0000

    small fix

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -1,14 +1,14 @@
-const eqArrays = function(arr1, arr2) {
-  for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  } return true;
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(arr1, arr2) {
+  if(eqArrays(arr1, arr2)){
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+  } else{
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }; //in js you cannot go annomous for the else statement;
 };
 
-const assertArraysEqual = function(actual, expected) {
-  return (actual === expected ?
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
-};
+module.exports = assertArraysEqual;
 
 /*to call the function of eqArrays to assertEqual, try this:
 store the eqArrays's output into a var - result:
@@ -21,6 +21,6 @@ replace result with what it is:eqArrays([], []);
 done.
 */
 
-assertArrayEqual(eqArrays([false], [false]), true);
-assertArrayEqual(eqArrays([1, 2, 3], [14, 44, 5]), false);
-assertArrayEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertArrayEqual(eqArrays([false], [false]), true);
+// assertArrayEqual(eqArrays([1, 2, 3], [14, 44, 5]), false);
+// assertArrayEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
