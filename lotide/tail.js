3d1ed8d38eae91d4c750b9c640e0dc5e034da94d
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-07-01 16:49:10 +0000

    debugged tail, thanks to Anchen

--- lotide/tail.js
+++ lotide/tail.js
@@ -1,12 +1,10 @@
-const assertEqual = function(actual, expected) {
-  return (actual === expected ?
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
-};
-const head = function(headArr) {
-  return (headArr === [] ? undefined : headArr[0]);
-};
 const tail = function(tailArr) {
-  return (tailArr === [] ? undefined : tailArr.slice[1]);
+  if(tailArr.length >= 1) {
+    return tailArr.slice(1);
+  }
+  return [];
 };
+
+
+module.exports = tail;
 
