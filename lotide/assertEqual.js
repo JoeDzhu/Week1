f2534a68c4ae79d954fe44e2d6077165750fc27e
Joe Dzhu <jiayi2.zhu@ryerson.ca>
2019-07-01 16:05:55 +0000

    modified head and assertEqual

--- lotide/assertEqual.js
+++ lotide/assertEqual.js
@@ -3,8 +3,10 @@ const assertEqual = function(actual, expected) {
   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
   : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(22, 31);
-assertEqual(false,true);
-assertEqual("TIme", "TIME");
+module.exports = assertEqual;
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(22, 31);
+// assertEqual(false,true);
+// assertEqual("TIme", "TIME");
--- lotide/head.js
+++ lotide/head.js
@@ -1,11 +1,7 @@
-const assertEqual = function(actual, expected) {
-  return (actual === expected ?
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
-};
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
   return (arr === [] ? undefined : arr[0]);
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
